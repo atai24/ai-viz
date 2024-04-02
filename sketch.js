99179e4c359fa77fdd01bf5d67c477f193040f17
Alex Tai <alex.tai@parivedasolutions.com>
2024-04-02 17:08:09 -0500

    testing webcam input

--- sketch.js
+++ sketch.js
@@ -1,38 +1,22 @@
-/*
- * @name Wavemaker
- * @arialabel Water like waves of neon green lines moving in circular patterns. The user’s mouse can change the direction of the current in the waves
- * @description This illustrates how waves (like water waves) emerge
- * from particles oscillating in place. Move your mouse to direct the wave.
- * Contributed by Aatish Bhatia, inspired by <a href="https://beesandbombs.tumblr.com/post/45513650541/orbiters">Orbiters</a> by Dave Whyte.
- */
-
-let t = 0; // time variable
-
+let cap;
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 400);
+  cap = createCapture(VIDEO);
+  cap.hide();
+  rectMode(CENTER);
   noStroke();
-  fill(40, 200, 40);
 }
-
 function draw() {
-  background(10, 10); // translucent background (creates trails)
-
-  // make a x and y grid of ellipses
-  for (let x = 0; x <= width; x = x + 30) {
-    for (let y = 0; y <= height; y = y + 30) {
-      // starting point of each circle depends on mouse position
-      const xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
-      const yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
-      // and also varies based on the particle's location
-      const angle = xAngle * (x / width) + yAngle * (y / height);
-
-      // each particle moves in a circle
-      const myX = x + 20 * cos(2 * PI * t + angle);
-      const myY = y + 20 * sin(2 * PI * t + angle);
-
-      ellipse(myX, myY, 10); // draw particle
+  background(50);
+  fill(255);
+  cap.loadPixels();
+  for (let cy = 0; cy < cap.height; cy += 10) {
+    for (let cx = 0; cx < cap.width; cx += 5) {
+      let offset = ((cy*cap.width)+cx)*4;
+      let xpos = (cx / cap.width) * width;
+      let ypos = (cy / cap.height) * height;
+      rect(xpos, ypos, 10,
+        10 * (cap.pixels[offset+1]/255));
     }
   }
-
-  t = t + 0.01; // update time
 }
